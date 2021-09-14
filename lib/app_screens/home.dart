1c69bb9484ec82f38e68c1e0dc71fe12d8746428
Mohammed Rahil <mohdrahilansari@gmail.com>
2021-09-14 14:08:58 +0530

    Update home.dart
    added Raised Button and Alert Dialog Box

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -63,17 +63,58 @@ class Home extends StatelessWidget {
                     )),
                   ],
                 ),
-                  FlightImageAsset()
+                  FlightImageAsset(),
+                  FlightBookButton()
               ],
             )));
   }
 }
 
-class FlightImageAsset extends StatelessWidget{
+class FlightImageAsset extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AssetImage assetImage = AssetImage('images/flight.png');
-    Image image = Image(image: assetImage, width: 150.0, height: 150.0,);
-    return Container(child: image,);
+    Image image = Image(
+      image: assetImage,
+      width: 250.0,
+      height: 250.0,
+    );
+    return Container(
+      child: image,
+    );
+  }
+}
+
+class FlightBookButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+        color: Colors.deepOrange,
+        child: Text(
+          "Book Your Flight",
+          style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+              fontFamily: 'Ubranist',
+              fontWeight: FontWeight.w700),
+        ),
+        elevation: 6.0,
+        onPressed: () => bookFlight(context),
+      ),
+    );
+  }
+
+  void bookFlight(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Flight Booked Successfully"),
+      content: Text("Have a Pleasent Flight"),
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => alertDialog);
   }
 }
